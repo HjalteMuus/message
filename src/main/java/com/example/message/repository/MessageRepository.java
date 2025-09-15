057a1475809ce9cade20b9ea669d90ffe327e6bc
HjalteMuus <hjaltemb2@gmail.com>
2025-09-15 09:35:36 +0200

    add message from other project

---
+++ src/main/java/com/example/message/repository/MessageRepository.java
@@ -0,0 +1,47 @@
+package com.example.message.repository;
+
+import com.example.message.model.Message;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+// Repository-klassen fungerer som et datalager i hukommelsen.
+// Den opretter 3 beskeder ved opstart og returnerer dem via en metode
+public class MessageRepository {
+    private final List<Message> messages;
+    private int messageId = 1;
+
+    public MessageRepository() {
+        this.messages = new ArrayList<>();
+        //populateMessages();
+    }
+
+//    private void populateMessages() {
+//        while (messageId <= 3) {
+//            messages.add(new Message(messageId, "Velkommen til " + messageId + ".semester"));
+//            messageId++;
+//        }
+//    }
+
+    public List<Message> getAllMessages() {
+        return messages;
+    }
+
+    public Message findMessageById(int id) {
+        for (Message message : messages) {
+            if (message.getId() == id) {
+                return message;
+            }
+        }
+        return null;
+    }
+
+    public Message addMessage(Message message){
+        message.setId(messageId);
+        messages.add(message);
+        messageId++;
+        return message;
+    }
+}
